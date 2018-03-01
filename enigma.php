995460c44dab404e5903363c06c9154782aa30ac
Koolant <koolan413@gmail.com>
2018-02-28 22:00:55 -0800

    Add files via upload

--- enigma.php
+++ enigma.php
@@ -1,60 +1,47 @@
 <?php
 
-class Rotor
-{
-    public $alphabet;
-    public $wiring;
-    
-    public function __construct($wiring)
-    {
-        $this->alphabet = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        $this->wiring = str_split($wiring);
-    }
-    
-    public function rotateLeft()
-    {
-        array_push($this->alphabet, array_shift($this->alphabet));
-        array_push($this->wiring,array_shift($this->wiring));
-    }
-    
-    public function getOutIndex($idx)
-    {
-        $char = $this->alphabet[$idx];
-        return array_search($char, $this->wiring);
-    }
-    
-    public function getInIndex($idx)
-    {
-        $char = $this->wiring[$idx];
-        return array_search($char, $this->alphabet);
-    }
-}
-
 $alphabet = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
 $input = str_split("QHSGUWIG");
 $output = array();
 
+$plugboard = new PlugBoard(array("A"=>"B","S"=>"Z","U"=>"Y","G"=>"H","L"=>"Q","E"=>"N"));
+$reflector = new Rotor("YRUHQSLDPXNGOKMIEBFZCWVJAT","A");
 
-$reflector = new Rotor("YRUHQSLDPXNGOKMIEBFZCWVJAT");
+$scramblerTwo      = new Rotor("UWYGADFPVZBECKMTHXSLRINQOJ","E");
+$scrambler         = new Rotor("AJPCZWRLFBDKOTYUQGENHXMIVS","A");
+$scramblerThree    = new Rotor("TAGBPCSDQEUFVNZHYIXJWLRKOM","B");
 
-for ($i = 0;$i < count($alphabet); $i++) {
-    $scrambler = new Rotor("UWYGADFPVZBECKMTHXSLRINQOJ");
-    for ($j = 0;$j < $i;$j++) {
-        if($j>0){
-            $scrambler->rotateLeft();
-        }
-    }
+$counter = 0;
+
+while ($scramblerThree->counter < 26) {
+    $scrambler->reset();
+    $scramblerTwo->reset();
+    $scramblerThree->reset();
+
+    $scrambler->rotateLeft($counter);
+    $scramblerTwo->rotateLeft(floor($counter/25));
+    $scramblerThree->rotateLeft(floor($counter/50));
+
+    
     $output = [];
-    foreach ($input as $val) {
-        $scrambler->rotateLeft();
-        $stepOne = $scrambler->getOutIndex(array_search($val, $alphabet));
-        $stepTwo = $reflector->getOutIndex($stepOne);
-        $stepThree = $scrambler->getInIndex($stepTwo);
-        $output[] = $alphabet[$stepThree];
-    }
-    if($output[0].$output[1] !== 'XV') {
-        continue;
+    foreach ($input as $key => $val) {
+        $scrambler->rotateLeft(1);
+        $plugResult = $plugboard->getOut($val);
+        $a = $scrambler->getOutIndex(array_search($plugResult, $alphabet));
+        $b = $scramblerTwo->getOutIndex($a);
+        $c = $scramblerThree->getOutIndex($b);
+        $d = $reflector->getOutIndex($c);
+        $e = $scramblerThree->getInIndex($d);
+        $f = $scramblerTwo->getInIndex($e);
+        $g = $scrambler->getInIndex($f);
+        $result = $plugboard->getIn(array_search($g, $alphabet));
+
+        if ($result === $val) {
+            break;
+        }
+        $output[] = $alphabet[$g];
     }
     echo implode($output) . PHP_EOL;
+    $counter++;
 }
